3d163b664ecf7ff41bd811f90dbe9ab9cfbe5875
BasilKosovan <basil.kosovan@gmail.com>
2022-03-09 18:36:23 +0200

    initial commit

---
+++ RefOut.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class RefOut
+    {
+        public static void Execute()
+        {
+            var instanceVariable = new TestClass { MyProperty = 1 };
+            DoSmthQ1(instanceVariable);
+            //DoSmthQ2(instanceVariable);
+            //DoSmthQ3(instanceVariable);
+
+            instanceVariable.Print();
+        }
+
+
+        public static void DoSmthQ1(TestClass instanceVariableInMethod)
+        {
+            instanceVariableInMethod.MyProperty = 10;
+        }
+
+        public static void DoSmthQ2(TestClass instanceVariableInMethod)
+        {
+            instanceVariableInMethod = new TestClass { MyProperty = 10 };
+        }
+
+        public static void DoSmthQ3(ref TestClass instanceVariableInMethod)
+        {
+            instanceVariableInMethod = new TestClass { MyProperty = 10 };
+        }
+
+        public class TestClass
+        {
+            public int MyProperty { get; set; }
+
+            public void Print()
+            {
+                Console.WriteLine($"MyProperty = {MyProperty }");
+            }
+        }
+
+        //Stack: instanceVariable, instanceVariableInMethod
+        //Heap: object for instanceVariable and object ...
+    }
+}
---
+++ VirtualOverrideVsNew.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class VirtualOverrideVsNew
+    {
+        public static void Execute()
+        {
+            A a1 = new A();
+            A a2 = new B();
+            B b1 = new B();
+
+            a1.Print(); // 
+            a2.Print(); // 
+            b1.Print(); // 
+
+        }
+
+        public class A
+        {
+            public int MyProperty { get; set; }
+
+            public void Print()
+            {
+                Console.WriteLine($"{nameof(A)}");
+            }
+        }
+
+        public class B : A
+        {
+            public void Print()
+            {
+                Console.WriteLine($"{nameof(B)}");
+            }
+        }
+
+    }
+}
