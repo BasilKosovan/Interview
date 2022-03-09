df4958cbf5e7018654bc0193ac82b3a2d864fac2
BasilKosovan <basil.kosovan@gmail.com>
2022-03-09 20:16:21 +0200

    added async await vs task examples

---
+++ AsyncAwaitVsTask.cs
@@ -0,0 +1,58 @@
+﻿using RestSharp;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class AsyncAwaitVsTask
+    {
+        public static async Task Execute1()
+        {
+            Console.WriteLine($"Execute Before: {Thread.CurrentThread.ManagedThreadId}");
+            await AsyncAwaitClass.MakeFakeCallOnlyCPUAsync().ContinueWith(t =>
+            {
+                Calulate();
+
+            });
+            Console.WriteLine($"Execute After: {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        public static async Task Execute2()
+        {
+            Console.WriteLine($"Execute Before: {Thread.CurrentThread.ManagedThreadId}");
+            await AsyncAwaitClass.MakeFakeCallOnlyCPUAsync();
+
+            Calulate();
+
+            Console.WriteLine($"Execute After: {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        public static void Calulate()
+        {
+            Console.WriteLine($"Calulate Before Sleep: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(10);
+            Console.WriteLine($"Calulate After Sleep: {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        public class AsyncAwaitClass
+        {
+            public static async Task MakeCallAsync()
+            {
+                Console.WriteLine($"AsyncAwaitClass Before call: {Thread.CurrentThread.ManagedThreadId}");
+                var client = new RestClient("http://46.4.63.238/");
+                var request = new RestRequest("sites.json", Method.Get);
+                await client.GetAsync(request);
+                Console.WriteLine($"AsyncAwaitClass After call: {Thread.CurrentThread.ManagedThreadId}");
+            }
+
+            public static async Task MakeFakeCallOnlyCPUAsync()
+            {
+                Console.WriteLine($"AsyncAwaitClass Before call: {Thread.CurrentThread.ManagedThreadId}");
+                await Task.Delay(10);
+                Console.WriteLine($"AsyncAwaitClass After call: {Thread.CurrentThread.ManagedThreadId}");
+
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,19 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
 
             //VirtualOverrideVsNew.Execute();
-            RefOut.Execute();
+            //RefOut.Execute();
+
+            //await AsyncAwaitVsTask.Execute1();
+            await AsyncAwaitVsTask.Execute2();
+
 
             Console.ReadKey();
         }
